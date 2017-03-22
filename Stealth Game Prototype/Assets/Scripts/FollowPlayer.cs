7642c8f9176e2d120739955675e051be7aa6da3a
Drew Graham <drewgraham@Drews-MacBook-Pro.local>
2017-03-21 23:36:02 -0400

    Implemented player movement, basic enemy detection
    - player movement implemented (WASD + Shift to run)
    - basic enemy detection system implemented: there is a field around the player, and if it touches the enemy the player is detected

---
+++ Stealth Game Prototype/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour 
+{
+    private GameObject _player;
+	// Use this for initialization
+	void Start () 
+    {
+        _player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (gameObject.transform.position.x != _player.transform.position.x)
+            gameObject.transform.position = new Vector2(_player.transform.position.x, gameObject.transform.position.y);
+        if (gameObject.transform.position.y != _player.transform.position.y)
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x, _player.transform.position.y);
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Enemy")
+            Debug.Log("contact!");
+    }
+}
